099e0d6bff40f2b504e19437b8f9282b1d14c7a6
Alexandru Arion <alex.arion@gmail.com>
2024-02-08 15:44:16 +0200

    add support for odd/even page crops

--- pdf-utils/pdf-crop.py
+++ pdf-utils/pdf-crop.py
@@ -1,13 +1,17 @@
 import PyPDF2
 
-def crop_pdf(input_pdf, output_pdf, crop_box):
+def crop_pdf(input_pdf, output_pdf, crop_box_odd, crop_box_even):
     with open(input_pdf, 'rb') as file:
         pdf_reader = PyPDF2.PdfReader(file)
         pdf_writer = PyPDF2.PdfWriter()
 
         for idx, page in enumerate(pdf_reader.pages):
-            page.cropbox.lower_left = crop_box[0]
-            page.cropbox.upper_right = crop_box[1]
+            if idx % 2 == 0:
+                page.cropbox.lower_left = crop_box_odd[0]
+                page.cropbox.upper_right = crop_box_odd[1]
+            else:
+                page.cropbox.lower_left = crop_box_even[0]
+                page.cropbox.upper_right = crop_box_even[1]
             pdf_writer.add_page(page)
             # if idx == 30:
             #     break
@@ -16,10 +20,11 @@ def crop_pdf(input_pdf, output_pdf, crop_box):
             pdf_writer.write(output_file)
 
 # Example usage:
-input_pdf_path = 'input.pdf'
+input_pdf_path = r'input.pdf'
 output_pdf_path = 'output.pdf'
 
 # Crop box format: (lower_left_x, lower_left_y, upper_right_x, upper_right_y)
-crop_box = ((0, 100), (500, 690))
+crop_box_odd = ((90, 105), (590, 700))
+crop_box_even = ((20, 105), (520, 700))
 
-crop_pdf(input_pdf_path, output_pdf_path, crop_box)
+crop_pdf(input_pdf_path, output_pdf_path, crop_box_odd, crop_box_even)
